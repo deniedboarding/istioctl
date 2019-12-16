1b9512508dcbbc35eb9e992fca6e086760e6dd96
Mårten Svantesson <Marten.Svantesson@ticket.se>
2019-12-16 13:03:40 +0100

    PEK-727 SM-A Install Istio
    Fix download of istio

--- downloadIstio.sh
+++ downloadIstio.sh
@@ -3,16 +3,12 @@ OS="$(uname)"
 OSEXT="linux"
 
 NAME="istio-$VERSION"
+URL="https://github.com/istio/istio/releases/download/${VERSION}/istio-${VERSION}-${OSEXT}.tar.gz"
+echo "Downloading $NAME from $URL ..."
+curl -L "$URL" | tar xz
+# TODO: change this so the version is in the tgz/directory name (users trying multiple versions)
+echo "Downloaded into $NAME:"
+BINDIR="$(cd $NAME/bin; pwd)"
 
-ls $NAME > /dev/null 2>&1
-# if istio data is not download yet
-if [ $? != 0 ]; then
-  URL="https://github.com/istio/istio/releases/download/${VERSION}/istio-${VERSION}-${OSEXT}.tar.gz"
-  echo "Downloading $NAME from $URL ..."
-  curl -L "$URL" | tar xz
-  # TODO: change this so the version is in the tgz/directory name (users trying multiple versions)
-  echo "Downloaded into $NAME:"
-  BINDIR="$(cd $NAME/bin; pwd)"
+\cp -f $BINDIR/istioctl .
 
-  \cp -f $BINDIR/istioctl .
-fi
