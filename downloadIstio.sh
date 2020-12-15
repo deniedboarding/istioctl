d990aa49512da8c610e9bf206e6522f8f14ef162
Mårten Svantesson <Marten.Svantesson@ticket.se>
2020-12-15 15:08:06 +0100

    PEK-1779 Upgrade istio to latest version
    Build istioctl image for 1.8.1

--- downloadIstio.sh
+++ downloadIstio.sh
@@ -1,9 +1,7 @@
 #! /bin/sh
 NAME="istio-$VERSION"
-URL="https://github.com/istio/istio/releases/download/${VERSION}/istio-${VERSION}-linux.tar.gz"
+URL="https://storage.googleapis.com/istio-release/releases/$VERSION/istioctl-$VERSION-linux-amd64.tar.gz
 echo "Downloading $NAME from $URL ..."
 curl -sSL "$URL" | tar xz
-BINDIR="$(cd $NAME/bin; pwd)"
 
-\cp -f $BINDIR/istioctl .
 
